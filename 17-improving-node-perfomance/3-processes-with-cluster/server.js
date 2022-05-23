be3b968e52369effec33f861d49e948a9b6edf3f
Laureline <>
2022-05-23 14:30:33 +0200

    [  17_3_processes-with-cluster ] - built in cluster module allows to delegade server request over load by cloning the main process. --> thanks to OS module we can get how many cores we have in order to distribute the load.

---
+++ 17-improving-node-perfomance/3-processes-with-cluster/server.js
@@ -0,0 +1,66 @@
+const express 	= require( 'express' );
+const path 		= require( 'path' );
+const app 		= express();
+const cluster 	= require( 'cluster' );
+const OS		= require( 'os');
+
+
+
+
+const PORT 		= 3_000;
+let count		= 0;
+
+const pathResolve = ( _path ) => path.resolve( __dirname, _path );
+/**
+ * Delay using a while loop with a "startTime"
+ * ( avoiding to use process handling timers ( from v8 ))
+ * @param {number} duration 
+ */
+function delay( duration ){
+	const startTime = new Date();
+
+	while( new Date() - startTime < duration  ){
+		count += 1;
+		console.log(`Within loop...: ${ count }`);
+		
+		/** Event loop & CPU is blocked processing
+		 * 	this loop
+		 * **/
+	}
+}
+
+
+/* Minimal server with routes */
+app
+	.get('/', (req, res) => {
+		console.log('process.pid', process.pid )
+		res.sendFile( pathResolve( '../pages/index.html' ));
+	})
+	/* 1 - mere blocking server */
+	.get( '/blocking-server', (req, res) => {
+		delay(9_000);
+		res.sendFile( pathResolve( '../pages/blocking-server.html' ) );
+	})
+	/** 2 - blocking functions [ JSON [ stringify, parse ]] */
+	.get('/blocking-functions', (req, res) => {
+		/**
+		 * - JSON.stringify();
+		 * - JSON.parse();
+		 * - Crypto module [ methods: pbkdf2, scrypt ]
+		 *  */ 
+		res.sendFile( pathResolve( '../pages/blocking-functions.html' ));
+	})
+
+if( cluster.isMaster ){
+	console.log(' Master process has been started: ',  process.pid);
+	const NUM_WORKERS = OS.cpus().length;
+	// dynamically creates new cluster ( clone process ) based on available cores on the machine
+	for( let i = 0; i < NUM_WORKERS; i++ ){
+		cluster.fork();
+	}
+} else {
+	console.log( 'Process worker running', process.pid );
+	app.listen( PORT, () => {
+		console.log(`Donna is listening to http://localhost:${  PORT }`);
+	});
+}
