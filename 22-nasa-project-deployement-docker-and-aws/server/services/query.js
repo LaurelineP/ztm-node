63cdccabed99e6c2285d58cc18da2b1a76121945
Laureline Paris <32878345+LaurelineP@users.noreply.github.com>
2023-06-16 20:13:58 +0200

    Update query.js
    Code lines: align syntax

--- 22-nasa-project-deployement-docker-and-aws/server/services/query.js
+++ 22-nasa-project-deployement-docker-and-aws/server/services/query.js
@@ -1,7 +1,7 @@
 function getQuerySkip({ limitRaw, pageRaw } = { limitRaw: 0, pageRaw: 0 }){
-	const page 		= Math.abs( pageRaw );
+	const page	= Math.abs( pageRaw );
 	const limit 	= Math.abs( limitRaw );
-	const skip 		= ( page - 1 ) * limit;
+	const skip 	= ( page - 1 ) * limit;
 	return {
 		limit,
 		skip
